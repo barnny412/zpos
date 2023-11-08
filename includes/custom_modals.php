71a3c28d54345f15b26d4cfe224d79371cd8830e
Barnny412 <barnny412@gmail.com>
2023-11-08 15:11:47 +0200

    Add sales returns form and processing functionality
    This commit introduces a new HTML form for handling sales returns. Users can input return details, including the original sales ID, product ID, quantity returned, and a reason for the return. The form data is processed on the server-side using PHP, and sales return records are inserted into the 'SalesReturns' table in the database. Additionally, the inventory is adjusted for the returned products in the 'Products' table.

--- includes/custom_modals.php
+++ includes/custom_modals.php
@@ -8,7 +8,29 @@
                 <button id="cancel-cash-button">Cancel</button>
             </div>
         </div>   
-    </div>
+</div>
+
+<div class="custom-modal" id="sales-return-modal">
+        <div class="modal-container">
+        <h1>Sales Returns Form</h1>
+        <form action="process_return.php" method="post">
+            <label for="original_sales_id">Original Sales ID:</label>
+            <input type="text" name="original_sales_id" id="original_sales_id" required><br>
+
+            <label for="product_id">Product ID:</label>
+            <input type="text" name="product_id" id="product_id" required><br>
+
+            <label for="quantity_returned">Quantity Returned:</label>
+            <input type="number" name="quantity_returned" id="quantity_returned" required><br>
+
+            <label for="reason">Reason for Return:</label>
+            <textarea name="reason" id="reason" required></textarea><br>
+
+            <input type="submit" value="Submit">
+        </form>
+        </div>   
+</div>
+
     <div class="custom-alert" id="custom-alert">
         <div class="alert-content">
             <h2>Payment Successful!</h2>
